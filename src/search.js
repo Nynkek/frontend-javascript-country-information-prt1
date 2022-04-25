1b90deec956056dc330fcdcaebb2ee1b09f61b62
nynkek <nyn.jaa@gmail.com>
2022-04-25 14:54:22 +0200

    alles af. nu op naar de bonus!

--- src/search.js
+++ src/search.js
@@ -2,42 +2,40 @@ import axios from 'axios';
 
 
 const countryInfo = document.getElementById('country-info');
-// const countryName = document.getElementById('countryName-input');
 const search = document.getElementById('search-btn');
 const form = document.getElementById('country-form');
 
-async function fetchCountry(countryName) {
+async function fetchCountry(event) {
+    event.preventDefault();
+
     try {
-        // het request maken
+        let countryName = document.getElementById('countryName-input').value;
         const response = await axios.get("https://restcountries.com/v2/name/" + countryName);
-        console.log(response.data[0]);
         const {name, flags: {png}, subregion, population, capital, currencies, languages} = response.data[0];
-        countryInfo.innerHTML = `<h2><img src="${png}" class="flag-img" alt="vlag van ${name}">${name}</h2><p>${name} is situated in ${subregion}. It has a population of ${population.toLocaleString('nl-NL')} people. <br>
-        The capital is ${capital} and you can pay with ${currencies[0].name}'s.<br>
-       They speak ${languages[0].name}</p>`;
+        console.log(currencies);
+        let secondCurrency = '';
+        if (Object.keys(currencies).length > 1) {
+            secondCurrency = " and " + currencies[1].name;
+        }
+
+
+        countryInfo.innerHTML =
+            `<h2><img src="${png}" class="flag-img" alt="vlag van ${name}">${name}</h2>
+             <p>${name} is situated in ${subregion}. 
+             It has a population of ${population.toLocaleString('nl-NL')} people. <br>
+             The capital is ${capital} and you can pay with ${currencies[0].name}'s${secondCurrency}.<br>
+             They speak ${languages[0].name}</p>
+             `;
+        form.reset();
+
     } catch (e) {
-        // de errors
         console.error(e);
+        countryInfo.innerHTML = `<p>Je hebt een verkeerde naam ingevoerd. <br>Probeer het nog eens met een andere spelling. Of met bijvoorbeeld: Nederland.</p>`;
     }
 }
 
-// fetchCountry();
 
-function searchCountry(event) {
-    event.preventDefault()
-    let countryName = document.getElementById('countryName-input').value;
-    console.log(countryName + " naam")
-    fetchCountry(countryName);
-}
-
-form.addEventListener('submit', searchCountry);
-// search.addEventListener('click', searchCountry);
-
-// countryName.addEventListener('keyup', function (event) {
-//     if (event.code == 13) {
-//         event.preventDefault();
-//     }
-// });
+form.addEventListener('submit', fetchCountry);
 
 
 // maak een connectie met 1 land
